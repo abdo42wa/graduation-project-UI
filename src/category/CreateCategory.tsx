90a284bb80461ed2c2fbcd8e3496559d1926fb1a
Abdo Elsayed <abdo.ahmed.aa754@gmail.com>
2022-10-28 23:53:47 +0300

    add category page

--- src/category/CreateCategory.tsx
+++ src/category/CreateCategory.tsx
@@ -1,10 +1,11 @@
-import { Typography } from "@mui/material"
+import { TextField, Typography } from "@mui/material"
 import { Box } from "@mui/system"
 
 export const CreateCategory = () => {
   return (
     <Box>
-        <Typography variant="h1">Create Category/ subCategory</Typography>
+        <Typography variant="h1">Category</Typography>
+        <TextField id="standard-basic" label="Category Name" variant="standard"/>
     </Box>
   )
 }
